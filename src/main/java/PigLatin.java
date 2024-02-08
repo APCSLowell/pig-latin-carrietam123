5d845cb1843ab2ef628116d7ceb6831f0b288f93
carrietam123 <157089313+carrietam123@users.noreply.github.com>
2024-02-07 21:01:42 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -41,19 +41,18 @@ return i;
 	    if(findFirstVowel(sWord) == -1) {
 		    return sWord + "ay";
 	    }
-	    else {
-		return "ERROR!";
-	    }
-	    if(sWord.substring(0,2).equals("qu")){
+	    else if(sWord.substring(0,2).equals("qu")){
 		    return(sWord.substring(2, sWord.length())+"quay");
 	    }
-	if(sWord.substring( 0,1).equals("e") || sWord.substring( 0,1).equals("a") || sWord.substring( 0,1).equals("i") ||sWord.substring( 0,1).equals("o") ||sWord.substring(0,1).equals("u") ){
+	else if(sWord.substring( 0,1).equals("e") || sWord.substring( 0,1).equals("a") || sWord.substring( 0,1).equals("i") ||sWord.substring( 0,1).equals("o") ||sWord.substring(0,1).equals("u") ){
 			return(sWord + "way");}
 		 for(int i = 0; i < sWord.length(); i++){
    			 if(!sWord.substring( i,i+1).equals("e") || !sWord.substring( i,i+1).equals("a") || !sWord.substring( i,i+1).equals("i") ||!sWord.substring( i,i+1).equals("o") || !sWord.substring( i,i+1).equals("u") ){
     	return sWord + "ay";}
 		 }
-	    
+	    else {
+		return "ERROR!";
+	    }
 		 }
    
 	
